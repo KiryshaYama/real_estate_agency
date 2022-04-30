71e3a29b86d2792434f3b871102cb0f86c430e61
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-30 22:49:33 +0500

    add db model for сomplaints

--- property/admin.py
+++ property/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Flat
-
+from .models import Complaint
 
 class FlatAdmin(admin.ModelAdmin):
     search_fields = (
@@ -18,4 +18,8 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
 
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat')
+
 admin.site.register(Flat, FlatAdmin)
+admin.site.register(Complaint, ComplaintAdmin)
--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -50,5 +51,25 @@ class Flat(models.Model):
         'Новостройка',
         db_index=True)
 
-    def __str__(self):
+class Complaint(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='complaints',
+        verbose_name='Кто жаловался',
+        on_delete=models.CASCADE
+    )
+    flat = models.ForeignKey(
+        Flat,
+        related_name='complaints',
+        verbose_name='Квартира, на которую пожаловались',
+        on_delete=models.CASCADE
+    )
+
+    text = models.TextField(
+        'Текст жалобы',
+        max_length=2000
+    )
+
+
+def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
