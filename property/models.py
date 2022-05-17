74d3cdf58d8b308b0c68363ad75586fa2f330451
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-05-17 10:21:10 +0500

    add blank to Owner.flats

--- property/models.py
+++ property/models.py
@@ -88,7 +88,8 @@ class Owner(models.Model):
     flats = models.ManyToManyField(
         Flat,
         related_name='owners',
-        verbose_name='Квартиры в собственности')
+        verbose_name='Квартиры в собственности',
+        blank=True)
 
     def __str__(self):
         return self.fullname
